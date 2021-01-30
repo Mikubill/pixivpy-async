e866816e9729503913507abead06625ac02ad17d
LuneZ99 <lune_z@foxmail.com>
2021-01-30 22:43:07 +0800

    Add a proxy connector supports SOCKS4 / SOCKS5 / HTTP, and make it possible to specify the proxy server manually.

--- pixivpy_async/client.py
+++ pixivpy_async/client.py
@@ -1,10 +1,46 @@
 import asyncio
 import aiohttp
+from aiohttp_socks import ProxyType, ProxyConnector, ChainProxyConnector
 
 
 class PixivClient:
-    def __init__(self, limit=30, timeout=10, env=False, internal=False):
-        self.conn = aiohttp.TCPConnector(limit_per_host=limit)
+    def __init__(self, limit=30, timeout=10, env=False, internal=False, proxy=None):
+        """
+            When 'env' is True and 'proxy' is None, possible proxies will be
+            obtained automatically (wrong proxy may be obtained).
+
+            When 'proxy' is not None, it will force the proxy to be used and
+            'env' will have no effect.
+
+            proxy <str> is used for a single proxy with a url:
+                'socks5://user:password@127.0.0.1:1080'
+
+            proxy <dict> is used for a single proxy with a dict:
+                proxy_dict = {
+                    'proxy_type': ProxyType.SOCKS5,
+                    'host': '127.0.0.1',
+                    'port': 1080,
+                    'rdns': True
+                }
+
+            proxy <list> is used for proxy chaining with a list of urls:
+                [
+                    'socks5://user:password@127.0.0.1:1080',
+                    'socks4://127.0.0.1:1081',
+                    'http://user:password@127.0.0.1:3128',
+                ]
+
+        """
+
+        if type(proxy) is str:
+            self.conn = ProxyConnector.from_url(proxy, limit_per_host=limit)
+        if type(proxy) is dict:
+            self.conn = ProxyConnector(**proxy, limit_per_host=limit)
+        if type(proxy) is list:
+            self.conn = ChainProxyConnector.from_urls(proxy, limit_per_host=limit)
+        if not proxy:
+            self.conn = aiohttp.TCPConnector(limit_per_host=limit)
+
         self.internal = internal
         self.client = aiohttp.ClientSession(
             connector=self.conn,
