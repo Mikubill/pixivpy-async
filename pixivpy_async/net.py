5275a26ac0edff4b8eb146759ac06f74aa775b06
Miyako Satori <31246794+Mikubill@users.noreply.github.com>
2021-02-02 18:46:04 +0900

    add proxy for net object

--- pixivpy_async/net.py
+++ pixivpy_async/net.py
@@ -9,9 +9,9 @@ retryable_error = asyncio.TimeoutError, aiohttp.ClientError, aiohttp.ServerTimeo
 
 
 class ClientManager:
-    def __init__(self, s, env):
+    def __init__(self, s, env, proxy):
         if s is None:
-            self.session = PixivClient(internal=True, env=env)
+            self.session = PixivClient(internal=True, env=env, proxy=proxy)
         else:
             self.session = s
 
@@ -28,14 +28,15 @@ class ClientManager:
 
 
 class Net(object):
-    def __init__(self, client=None, env=False, **requests_kwargs):
+    def __init__(self, client=None, env=False, proxy=None, **requests_kwargs):
         self.session = client
         self.env = env
+        self.proxy = proxy
         self.requests_kwargs = requests_kwargs
 
     @retry(*retryable_error, retries=10, cooldown=random.randint(1, 3))
     async def down(self, _url, _referer):
-        async with ClientManager(self.session, self.env) as session:
+        async with ClientManager(self.session, self.env, self.proxy) as session:
             async with session.get(_url, headers={'Referer': _referer}, **self.requests_kwargs) as res:
                 c = await res.read()
                 t = res.content_type
@@ -45,7 +46,7 @@ class Net(object):
 
     @retry(*retryable_error, retries=10, cooldown=random.randint(1, 3))
     async def auth(self, _url, _headers, _data):
-        async with ClientManager(self.session, self.env) as session:
+        async with ClientManager(self.session, self.env, self.proxy) as session:
             async with session.post(_url, headers=_headers, data=_data, **self.requests_kwargs) as res:
                 r, b, q = await res.json(), res.status in [200, 301, 302], res.status
 
@@ -54,7 +55,7 @@ class Net(object):
 
     @retry(*retryable_error, retries=10, cooldown=random.randint(1, 3))
     async def fetch(self, _url, _headers, _params):
-        async with ClientManager(self.session, self.env) as session:
+        async with ClientManager(self.session, self.env, self.proxy) as session:
             async with session.get(_url, headers=_headers, params=_params, **self.requests_kwargs) as res:
                 q = await res.json()
 
@@ -63,7 +64,7 @@ class Net(object):
 
     @retry(*retryable_error, retries=10, cooldown=random.randint(1, 3))
     async def post(self, _url, _data, _headers, _params):
-        async with ClientManager(self.session, self.env) as session:
+        async with ClientManager(self.session, self.env, self.proxy) as session:
             async with session.post(_url, data=_data, headers=_headers, params=_params, **self.requests_kwargs) as res:
                 r = await res.json()
 
@@ -72,7 +73,7 @@ class Net(object):
 
     @retry(*retryable_error, retries=10, cooldown=random.randint(1, 3))
     async def delete(self, _url, _headers, _params):
-        async with ClientManager(self.session, self.env) as session:
+        async with ClientManager(self.session, self.env, self.proxy) as session:
             async with session.delete(_url, headers=_headers, params=_params, **self.requests_kwargs) as res:
                 q = await res.json()
 
